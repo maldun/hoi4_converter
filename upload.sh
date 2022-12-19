d1c4993704a46f55f1492de44ee7292bd1c9e108
Mal'dun Finsterschreck <Stefan Reiterer>
2022-12-19 19:55:47 +0100

    updated upload description

--- upload.sh
+++ upload.sh
@@ -1,3 +1,13 @@
 # pip install --user twine
+
+# upload to test
 python setup.py sdist
 twine upload dist/* -r pypitest
+
+# upload to pypi
+python setup.py sdist bdist_wheel --universal
+twine upload dist/* -r pypi
+
+# add tag to git
+git tag -a v1.0.0 -m "annotation for this release"
+git push origin --tags
