58f37d52e4530611fe02af08eb23ff006c3174d6
Mal'dun Finsterschreck <Stefan Reiterer>
2022-12-19 18:50:57 +0100

    added pakageing stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'hoi4_converter',
+    packages = ['Hoi4Converter'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'HOI4 Converter: A Python package for easier HOI4 Modding',
+    author = 'See Github',
+    author_email = 'See Github',
+    url = 'https://github.com/maldun/hoi4_converter',
+    download_url = 'download link you saved',
+    keywords = ['modding', 'Paradox'],
+    classifiers = [],
+) 
